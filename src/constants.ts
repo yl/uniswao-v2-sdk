ba40746746ceb32da1b921293c36f4c75431809f
yangliu <yangliulnn@gmail.com>
2021-12-23 21:51:40 +0800

    Use environment variables to replace hard-coded, add test chain id.

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  LOCAL = 31337
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = process.env.REACT_APP_FACTORY as string
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = process.env.REACT_APP_INIT_CODE_HASH as string
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
